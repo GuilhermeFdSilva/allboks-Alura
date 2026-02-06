dabde6ae199898d886745c1dc598aaaf0cfe104c
Guilherme França da Silva <francaguilherme27@gmail.com>
2026-02-06 13:49:16 -0300

    Altera server

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const server = require('./server');
 
 server.listen(8000, () => {
-    console.log("Acesse essa API em http://localhost:8000")
+    console.log("API disponível em http://localhost:8000")
 });
